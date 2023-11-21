70f8f00f1874aa783b98494e0f57d4bc102891bf
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-21 16:06:44 +0100

    feat: add /tickets route that uses postTickets

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getAreaInfo, listAreas } from "../controllers/event-controller.js";
+import {
+  getAreaInfo,
+  listAreas,
+  postTickets,
+} from "../controllers/event-controller.js";
 
 export const router = express.Router();
 
 // Routes here
 router.route("/areas/").get(listAreas);
 router.route("/details/:area").get(getAreaInfo);
+router.route("/tickets").post(postTickets);
